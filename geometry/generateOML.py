c89dbfb8a5b4506bd70ee6d890661fbdf0120a49
Alasdair Gray <alachris@umich.edu>
2024-06-14 19:33:19 -0400

    More advanced FFD that meets parameterisation requirements

--- geometry/generateOML.py
+++ geometry/generateOML.py
@@ -26,8 +26,11 @@ import numpy as np
 # ==============================================================================
 from wingGeometry import wingGeometry
 
+spanIndex = wingGeometry["spanIndex"]
+chordIndex = wingGeometry["chordIndex"]
+
 parser = argparse.ArgumentParser()
-parser.add_argument("--ffdType", type=str, choices=["basic", "advanced", "none"], default="basic")
+parser.add_argument("--ffdType", type=str, choices=["basic", "advanced", "none"], default="advanced")
 args = parser.parse_args()
 
 airfoilFiles = [
@@ -59,22 +62,22 @@ wingSurface.writeTecplot("wing.dat")
 wingSurface.writeIGES("wing.igs")
 wingSurface.writeTin("wing.tin")
 
+numFFDSpan = [6, 9, 12]
+numFFDChord = [8, 12, 16]
+
+# --- We need to shift the LE and TE coordinate slightly inside the wing for the fitted FFD creation to work ---
+wingLEList = wingGeometry["wing"]["LECoords"]
+wingTEList = wingGeometry["wing"]["TECoords"]
+wingLEList += 0.01 * (wingTEList - wingLEList)
+wingTEList += 0.01 * (wingLEList - wingTEList)
+wingLEList[0, spanIndex] += 1e-3
+wingTEList[0, spanIndex] += 1e-3
+
 if args.ffdType == "basic":
     # ==============================================================================
     # Generate fitted FFD
     # ==============================================================================
-    numFFDSpan = [6, 9, 12]
-    numFFDChord = [8, 12, 16]
     fileNames = ["wing-ffd-coarse.xyz", "wing-ffd-med.xyz", "wing-ffd-fine.xyz"]
-
-    # --- We need to shift the LE and TE coordinate slightly inside the wing for the fitted FFD creation to work ---
-    wingLEList = wingGeometry["wing"]["LECoords"]
-    wingTEList = wingGeometry["wing"]["TECoords"]
-    wingLEList += 0.01 * (wingTEList - wingLEList)
-    wingTEList += 0.01 * (wingLEList - wingTEList)
-    wingLEList[0, wingGeometry["spanIndex"]] += 1e-3
-    wingTEList[0, wingGeometry["spanIndex"]] += 1e-3
-
     for nSpan, nChord, file in zip(numFFDSpan, numFFDChord, fileNames):
         createFittedWingFFD(
             wingSurface,
@@ -84,8 +87,8 @@ if args.ffdType == "basic":
             teList=wingTEList,
             nSpan=nSpan,
             nChord=nChord,
-            absMargins=[0.05, 0.005, 0.05],
-            relMargins=[0.01, 0.005, 0.01],
+            absMargins=[0.01, 0.005, 0.05],
+            relMargins=[0.015, 0.0035, 0.01],
             liftIndex=wingGeometry["verticalIndex"] + 1,
         )
 
@@ -94,20 +97,21 @@ elif args.ffdType == "advanced":
     # Fancier FFD
     # ==============================================================================
     # This is a more complex FFD that should allw me to stop the wing root and SOB moving in the spanwise direction
+    fileNames = ["wing-ffd-advanced-coarse.xyz", "wing-ffd-advanced-med.xyz", "wing-ffd-advanced-fine.xyz"]
 
-    # The first segment of the FFD will contain 3 sections around the root, purely to enforce that the wing root does not
-    # move away from the symmetry plane
-    ffdLEList = np.zeros((4, 3))
-    ffdTEList = np.zeros((4, 3))
+    # The first segment of the FFD will contain 3 sections around the root, purely to enforce that the wing root does
+    # not move away from the symmetry plane
+    ffdLEList = np.zeros((6, 3))
+    ffdTEList = np.zeros((6, 3))
     ffdLEList[0] = wingLEList[0]
     ffdTEList[0] = wingTEList[0]
 
     ffdLEList[1] = ffdLEList[0]
     ffdTEList[1] = ffdTEList[0]
-    ffdLEList[1, wingGeometry["spanIndex"]] = 1e-2
-    ffdTEList[1, wingGeometry["spanIndex"]] = 1e-2
+    ffdLEList[1, spanIndex] = 1e-2
+    ffdTEList[1, spanIndex] = 1e-2
 
-    # Next we place an FFD section at the side-of-body location so we can fix that in place
+    # Next we place 3 FFD sections at the side-of-body location so we can fix that in place
     sobLE = wingLEList[0] + wingGeometry["wingbox"]["SOB"] / wingGeometry["wing"]["semiSpan"] * (
         wingLEList[1] - wingLEList[0]
     )
@@ -117,9 +121,17 @@ elif args.ffdType == "advanced":
     ffdLEList[2] = sobLE
     ffdTEList[2] = sobTE
 
+    ffdLEList[3] = ffdLEList[2] + 2.5e-4 * (ffdLEList[2] - ffdLEList[1])
+    ffdTEList[3] = ffdTEList[2] + 2.5e-4 * (ffdTEList[2] - ffdTEList[1])
+
+    ffdLEList[4] = ffdLEList[2] + 5e-4 * (ffdLEList[2] - ffdLEList[1])
+    ffdTEList[4] = ffdTEList[2] + 5e-4 * (ffdTEList[2] - ffdTEList[1])
+    # ffdLEList[3, spanIndex] += 1e-3
+    # ffdTEList[3, spanIndex] += 1e-3
+
     # The final FFD section is placed at the wing tip
-    ffdLEList[3] = wingLEList[-1]
-    ffdTEList[3] = wingTEList[-1]
+    ffdLEList[5] = wingLEList[-1]
+    ffdTEList[5] = wingTEList[-1]
 
     for nSpan, nChord, file in zip(numFFDSpan, numFFDChord, fileNames):
         createFittedWingFFD(
@@ -128,9 +140,9 @@ elif args.ffdType == "advanced":
             outFile=file,
             leList=ffdLEList,
             teList=ffdTEList,
-            nSpan=[2, 1, nSpan],
+            nSpan=[2, 1, 1, 1, nSpan],
             nChord=nChord,
-            absMargins=[0.05, 0.005, 0.05],
-            relMargins=[0.01, 0.005, 0.01],
+            absMargins=[0.01, 0.005, 0.05],
+            relMargins=[0.015, 0.0035, 0.01],
             liftIndex=wingGeometry["verticalIndex"] + 1,
         )
